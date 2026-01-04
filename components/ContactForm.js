8152e17d19cf2e8b4049d2f58e73b7b9ba425a1b
David Ansa <davitacols@gmail.com>
2026-01-04 21:35:15 +0100

    Remove address from contact information

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -50,16 +50,6 @@ export default function ContactForm() {
       <div className={styles.contactInfo}>
         <div className={styles.infoItem}>
           <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
-            <path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z"></path>
-            <circle cx="12" cy="10" r="3"></circle>
-          </svg>
-          <div>
-            <h4>Address</h4>
-            <p>London, United Kingdom</p>
-          </div>
-        </div>
-        <div className={styles.infoItem}>
-          <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
             <rect x="2" y="4" width="20" height="16" rx="2"></rect>
             <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"></path>
           </svg>
